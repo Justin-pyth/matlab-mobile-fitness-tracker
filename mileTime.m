9ad5f45637f91df440468df0340cd5fdfd376015
Justin-pyth <justinxsds357@gmail.com>
2025-07-12 16:53:13 -0700

    milteTime + totAlt + totDist

---
+++ mileTime.m
@@ -0,0 +1,25 @@
+clear
+load("/MATLAB Drive/matlab-mobile-fitness-tracker/Data/sensorlog_20250711_134319 Walking + standing still.mat")
+
+%Variable Initialization
+lat = Position.latitude;
+lon = Position.longitude;
+earthCirc = 24901;
+totaldis = 0;
+
+%Functions for Distance
+for i = 1:(length(lat)-1)
+    lat1 = lat(i);     % The first latitude
+    lat2 = lat(i+1);   % The second latitude
+    lon1 = lon(i);     % The first longitude
+    lon2 = lon(i+1);   % The second longitude
+    degDis = distance(lat1, lon1, lat2, lon2);
+    dis = (degDis/360)*earthCirc;
+    totaldis = totaldis + dis;
+end
+
+times = Position.Timestamp;
+time = timeElapsed(times);
+totTimeH = time(end,1)/3600;
+miletime = totaldis/totTimeH
+
---
+++ totAltitude.m
@@ -0,0 +1,4 @@
+clear
+load("sensorlog_20250711_134319 Walking + standing still.mat")
+
+highestAlt = max(Position.altitude)
