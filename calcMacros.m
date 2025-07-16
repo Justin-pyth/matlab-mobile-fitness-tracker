a6cf64ef1f4dc2fa6a3c5a7d226a87039ab744de
Justin-pyth <justinxsds357@gmail.com>
2025-07-16 10:29:39 -0700

    Fixed calcMacros + foodTrackerApp

--- calcMacros.m
+++ calcMacros.m
@@ -1,7 +1,6 @@
 function calories = calcMacros(foodData)
     %Calculates total calories and adds them to a counter based on the
     %given protein, fat, and carb data
-    clear;
-    calories = 4 * (foodData.protein + foodData.carbohydrates) + foodData.fat* 7;
+    calories = (4 * foodData.protein) + (4 * foodData.carbohydrates) + (foodData.fat * 7);
 
 end
