a573415729abc2fa35eaacf3c172e9adbc5483d7
Kevin Le <kevinle29299@gmail.com>
2025-07-21 19:14:15 -0700

    Move Activity Breakdown code to App Designer
    In the process, I removed most of the code in the
    FitnessTrackerModel script, leaving retrieval of
    training data(accel), classification Learner,
    and app starter.

--- FitnessTrackerModel.m
+++ FitnessTrackerModel.m
@@ -10,23 +10,6 @@ allAcceleration = timetable2table(allAcceleration, "ConvertRowTimes", false);
 %% Run classificationLeaner and test allAccelerations
 classificationLearner
 
-%% Train the model
-activity = load(fullfile('Data/MixedActivity/kevin_longrun1.mat'));
-justAcc = timetable2table(activity.Acceleration, "ConvertRowTimes", false); %remove timestamps
-yfit = trainedModel.predictFcn(justAcc); % train the model picked out from classficationLearner
-activity.Acceleration.Activity = yfit; % add activity vector label to activity
-
-% Calculate total calories burned
-weight = 86.1826; %I chose an arbitary value for weight
-totCaloriesBurned = calculateTotCaloriesBurned(weight,activity) 
-
-%% Plot on pie chart
-yfitcat = categorical(cellstr(yfit)); % convert to correct data type for histogram
-plotTotalCalBurned(totCaloriesBurned, yfitcat);
-
 %% Run the app and store user input
 app = FitnessTracker();
-
-% Load and assign values from userData struct
-load("userData.mat");
 
