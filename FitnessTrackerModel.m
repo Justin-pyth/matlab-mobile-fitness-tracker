cf854700f8e94e7adf5b92e2f713a542d2780270
Kevin Le <kevinle29299@gmail.com>
2025-07-24 01:54:38 -0700

    Create trainedModel from altitude using ML
    In addition, improved the getAltitudeGain() to
    compute accurate ouput.
    Performing machine learning on the altitude gain
    filters out the noise from gain from the phone motion
    during recording.

--- FitnessTrackerModel.m
+++ FitnessTrackerModel.m
@@ -7,6 +7,11 @@ clc;
 allAcceleration = getAllAccelerations();
 allAcceleration = timetable2table(allAcceleration, "ConvertRowTimes", false);
 
+%% Get all altitudes from folders and assign them to a variable
+allAltitudes = getAltitudeImproved();
+%allAltitudes.time = second(allAltitudes.Timestamp);
+allAltitudes = timetable2table(allAltitudes, "ConvertRowTimes", false);
+
 %% Run classificationLeaner and test allAccelerations
 classificationLearner
 
--- createLabel.m
+++ createLabel.m
@@ -1,13 +1,23 @@
-function label = createLabel(data, i)
+function label = createLabel(data, i, type)
     % This function adds a label to each activity 
-
-    if i == 1
-        label = 'running';
-    elseif i == 2
-        label = 'walking';
-    else
-        label = 'sitting';
+    
+    if type == "Acceleration"
+        if i == 1
+            label = 'running';
+        elseif i == 2
+            label = 'walking';
+        else
+            label = 'sitting';
+        end
+        label = repmat(label, size(data.Acceleration, 1), 1);
+    elseif type == "Position"
+        if i == 1
+            label = 'uphill';
+        elseif i == 2
+            label = 'downhill';
+        else
+            label = 'flat';
+        end
+        label = repmat(label, size(data.Position, 1), 1);
     end
-   
-    label = repmat(label, size(data.Acceleration, 1), 1);
 end
