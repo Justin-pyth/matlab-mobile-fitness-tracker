1df9ac330382f59a682e955679996e5c27eea1e3
Justin-pyth <justinxsds357@gmail.com>
2025-07-25 12:02:41 -0700

    Added functionality for list view of foods

--- HelperFunctions/calcMacros.m
+++ HelperFunctions/calcMacros.m
@@ -1,6 +1,5 @@
-function calories = calcMacros(foodData)
+function calories = calcMacros(protein, carbs, fat)
     %Calculates total calories and adds them to a counter based on the
     %given protein, fat, and carb data
-    calories = (4 * foodData.protein) + (4 * foodData.carbohydrates) + (foodData.fat * 7);
-
+    calories = (4 * protein) + (4 * carbs) + (7 * fat);
 end
