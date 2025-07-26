a2f44bdd0bd171dbea45ef4deaf2f4c3847e6dfe
Justin-pyth <justinxsds357@gmail.com>
2025-07-25 21:11:12 -0700

    FOODTRACKER DONE

--- HelperFunctions/removeFood.m
+++ HelperFunctions/removeFood.m
@@ -2,7 +2,7 @@ function removeFood(foodName)
     load("foodData.mat","foodData");    
     
     % Find and remove
-    idx = find(strcmp({foodData.name}, foodName));
+    idx = find(strcmp({foodData.name}, foodName), 1);
     if ~isempty(idx)
         foodData(idx) = [];
         save('foodData.mat', 'foodData');
